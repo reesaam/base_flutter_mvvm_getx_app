201f42dbdbe463841ddc733584394346e621e589
Resam Taghipour <resam.t@gmail.com>
2025-06-08 07:41:01 +0330

    GetPut Generated File

--- lib/main.get_put.dart
+++ lib/main.get_put.dart
@@ -1,3 +1,4 @@
+// dart format width=80
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
 // **************************************************************************
@@ -37,73 +38,74 @@ library;
 import 'package:get/get.dart';
 import 'main.dart';
 import 'admin/admin_app_countries/controller/admin_app_countries_controller.dart';
-import 'admin/admin_app_info/view/admin_app_info_page.dart';
-import 'admin/admin_data_format_check/view/admin_data_format_check_page.dart';
-import 'admin/admin_app_countries/view/admin_app_countries_page.dart';
-import 'admin/admin_app_resources/view/admin_app_resources_page.dart';
-import 'admin/admin_data_format_check/controller/admin_data_format_check_controller.dart';
-import 'admin/admin_start/controller/admin_start_controller.dart';
 import 'admin/admin_app_info/controller/admin_app_info_controller.dart';
 import 'admin/admin_app_resources/controller/admin_app_resources_controller.dart';
+import 'admin/admin_app_resources/view/admin_app_resources_page.dart';
+import 'admin/admin_data_format_check/controller/admin_data_format_check_controller.dart';
+import 'admin/admin_data_format_check/view/admin_data_format_check_page.dart';
+import 'admin/admin_start/controller/admin_start_controller.dart';
 import 'admin/admin_start/view/admin_start_page.dart';
-import 'admin/admin_test/view/admin_test_page.dart';
-import 'admin/admin_verifiers/view/admin_verifiers_page.dart';
-import 'admin/admin_verifiers/controller/admin_verifiers_controller.dart';
 import 'admin/admin_test/controller/admin_test_controller.dart';
-import 'admin/admin_widget_check/controller/admin_widget_check_controller.dart';
+import 'admin/admin_app_info/view/admin_app_info_page.dart';
+import 'admin/admin_verifiers/controller/admin_verifiers_controller.dart';
+import 'admin/admin_test/view/admin_test_page.dart';
 import 'admin/admin_widget_check/view/admin_widget_check_page.dart';
+import 'admin/admin_widget_check/controller/admin_widget_check_controller.dart';
+import 'admin/admin_verifiers/view/admin_verifiers_page.dart';
+import 'admin/admin_app_countries/view/admin_app_countries_page.dart';
 import 'components/connectivity/connectivity.dart';
+import 'components/failures/general_exception.dart';
 import 'components/failures/local_exception.dart';
 import 'components/failures/network_exception.dart';
+import 'components/share/share.dart';
+import 'components/permissions/permissions.dart';
 import 'components/file_functions/file_functions.dart';
 import 'components/network/dio.dart';
 import 'components/notifications/local_notifications/local_notifications.dart';
 import 'components/notifications/local_notifications/local_notification_controller.dart';
-import 'components/share/share.dart';
-import 'components/permissions/permissions.dart';
 import 'components/statistics/statistics.dart';
 import 'components/storage/app_storage_module.dart';
 import 'components/storage/storage_providers/app_local_storage.dart';
 import 'components/storage/storage_providers/app_shared_preferences.dart';
 import 'features/about/controller/about_controller.dart';
 import 'features/about/view/about_view.dart';
-import 'features/homepage/controller/homepage_controller.dart';
 import 'features/homepage/view/homepage_view.dart';
 import 'features/not_found/controller/not_found_controller.dart';
-import 'features/not_found/view/not_found_view.dart';
+import 'features/homepage/controller/homepage_controller.dart';
 import 'features/settings/controller/settings_controller.dart';
+import 'features/not_found/view/not_found_view.dart';
 import 'features/settings/view/settings_view.dart';
 import 'features/splash_screen/controller/splash_screen_controller.dart';
 import 'features/splash_screen/view/splash_screen_view.dart';
 import 'features/update/controller/update_controller.dart';
 import 'features/update/data/update_remote_data_source.dart';
 import 'features/update/view/update_view.dart';
-import 'localization/localizations.dart';
 import 'features/versions/data/versions_local_data_source.dart';
+import 'localization/localizations.dart';
 import 'features/versions/data/versions_remote_data_source.dart';
 import 'ui_kit/theme/themes.dart';
 
 /// Generated Library Statistics:
-///   Imports Count: 46
+///   Imports Count: 47
 ///   Pages Count: 14
 ///   Controllers Count: 15
-///   Components Count: 14
+///   Components Count: 15
 ///   Repositories Count: 3
 
 class GetPutPages {
   static List<GetPage> get pages => [
-        GetPage(name: '/AdminAppInfoPage', page: AdminAppInfoPage.new),
+        GetPage(
+            name: '/AdminAppResourcesPage', page: AdminAppResourcesPage.new),
         GetPage(
             name: '/AdminDataFormatCheckPage',
             page: AdminDataFormatCheckPage.new),
+        GetPage(name: '/AdminStartPage', page: AdminStartPage.new),
+        GetPage(name: '/AdminAppInfoPage', page: AdminAppInfoPage.new),
+        GetPage(name: '/AdminTestPage', page: AdminTestPage.new),
+        GetPage(name: '/AdminWidgetCheckPage', page: AdminWidgetCheckPage.new),
+        GetPage(name: '/AdminVerifiersPage', page: AdminVerifiersPage.new),
         GetPage(
             name: '/AdminAppCountriesPage', page: AdminAppCountriesPage.new),
-        GetPage(
-            name: '/AdminAppResourcesPage', page: AdminAppResourcesPage.new),
-        GetPage(name: '/AdminStartPage', page: AdminStartPage.new),
-        GetPage(name: '/AdminTestPage', page: AdminTestPage.new),
-        GetPage(name: '/AdminVerifiersPage', page: AdminVerifiersPage.new),
-        GetPage(name: '/AdminWidgetCheckPage', page: AdminWidgetCheckPage.new),
         GetPage(name: '/AboutPage', page: AboutPage.new),
         GetPage(name: '/HomePage', page: HomePage.new),
         GetPage(name: '/NotFoundPage', page: NotFoundPage.new),
@@ -132,27 +134,27 @@ class _GetPutController extends Bindings {
     Get.lazyPut<AdminAppCountriesController>(
         () => AdminAppCountriesController(),
         fenix: true);
-    Get.lazyPut<AdminDataFormatCheckController>(
-        () => AdminDataFormatCheckController(),
-        fenix: true);
-    Get.lazyPut<AdminStartController>(() => AdminStartController(),
-        fenix: true);
     Get.lazyPut<AdminAppInfoController>(() => AdminAppInfoController(),
         fenix: true);
     Get.lazyPut<AdminAppResourcesController>(
         () => AdminAppResourcesController(),
         fenix: true);
-    Get.lazyPut<AdminVerifiersController>(() => AdminVerifiersController(),
+    Get.lazyPut<AdminDataFormatCheckController>(
+        () => AdminDataFormatCheckController(),
+        fenix: true);
+    Get.lazyPut<AdminStartController>(() => AdminStartController(),
         fenix: true);
     Get.lazyPut<AdminTestController>(() => AdminTestController(), fenix: true);
+    Get.lazyPut<AdminVerifiersController>(() => AdminVerifiersController(),
+        fenix: true);
     Get.lazyPut<AdminWidgetCheckController>(() => AdminWidgetCheckController(),
         fenix: true);
     Get.lazyPut<AppLocalNotificationController>(
         () => AppLocalNotificationController(),
         fenix: true);
     Get.lazyPut<AboutController>(() => AboutController(), fenix: true);
-    Get.lazyPut<HomePageController>(() => HomePageController(), fenix: true);
     Get.lazyPut<NotFoundController>(() => NotFoundController(), fenix: true);
+    Get.lazyPut<HomePageController>(() => HomePageController(), fenix: true);
     Get.lazyPut<SettingsController>(() => SettingsController(), fenix: true);
     Get.lazyPut<SplashScreenController>(() => SplashScreenController(),
         fenix: true);
@@ -165,14 +167,15 @@ class _GetPutComponent extends Bindings {
   void dependencies() {
     Get.lazyPut<AppConnectionChecker>(() => AppConnectionChecker(),
         fenix: true);
+    Get.lazyPut<GeneralException>(() => GeneralException(), fenix: true);
     Get.lazyPut<LocalException>(() => LocalException(), fenix: true);
     Get.lazyPut<NetworkException>(() => NetworkException(), fenix: true);
+    Get.lazyPut<AppShare>(() => AppShare(), fenix: true);
+    Get.lazyPut<AppPermissions>(() => AppPermissions(), fenix: true);
     Get.lazyPut<AppFileFunctions>(() => AppFileFunctions(), fenix: true);
     Get.lazyPut<DioCore>(() => DioCore(), fenix: true);
     Get.lazyPut<AppLocalNotifications>(() => AppLocalNotifications(),
         fenix: true);
-    Get.lazyPut<AppShare>(() => AppShare(), fenix: true);
-    Get.lazyPut<AppPermissions>(() => AppPermissions(), fenix: true);
     Get.lazyPut<AppStatistics>(() => AppStatistics(), fenix: true);
     Get.lazyPut<AppStorage>(() => AppStorage(), fenix: true);
     Get.lazyPut<AppLocalStorage>(() => AppLocalStorage(), fenix: true);
